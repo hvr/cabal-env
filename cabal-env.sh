f8afd958e0f71d5cce44b2183f939d578ca5877d
Herbert Valerio Riedel <hvr@gnu.org>
2017-11-05 14:00:52 +0100

    Add support for listing/removing env files

--- cabal-env.sh
+++ cabal-env.sh
@@ -4,10 +4,14 @@ set -e
 
 show_usage () {
     cat <<EOF
-Usage: $0 [-n <env-name>] [DEP]+"
-    
+Usage: $0 [-n <env-name>] (-r | -l | [DEP]+)"
+
 Creates GHC package environment named '<env-name>' (default = 'default')
 containing the specified deps.
+
+  -r deletes the specified environment
+
+  -l lists available environments for current 'ghc'
 
 Examples:
 
@@ -19,11 +23,32 @@ EOF
     exit 1
 }
 
+get_ghc_triplet () {
+    GHC_TARGET=$(ghc --print-target-platform)
+    GHC_VER=$(ghc --numeric-version)
+    trip_pat="([^-]*)-([^-]*)-(.*)"
+
+    [[ $GHC_TARGET =~ $trip_pat ]]
+
+    TRIPLET="${BASH_REMATCH[1]}-${BASH_REMATCH[3]}-${GHC_VER}"
+
+    ENVSDIR="${HOME}/.ghc/${TRIPLET}/environments/"
+}
+
+remove=false
+list=false
+
 envname="default"
 
-while getopts n: flag; do
+while getopts lrn: flag; do
     case $flag in
         n) envname="$OPTARG"
+           ;;
+
+        r) remove=true
+           ;;
+
+        l) list=true
            ;;
 
         ?) show_usage
@@ -32,6 +57,44 @@ while getopts n: flag; do
 done
 
 shift $((OPTIND - 1))
+
+if $list; then
+    if [ $# -ge 1 ]; then
+        show_usage
+    fi
+
+    get_ghc_triplet
+
+    if [ -d "$ENVSDIR" ]; then
+        echo "Environments available in $ENVSDIR:"
+        echo
+        cd "$ENVSDIR"
+        find . -type f | sed 's,^[.][/],- ,'
+    else
+        echo "Environment folder $ENVSDIR does not exist"
+    fi
+
+    exit 0
+fi
+
+if $remove; then
+    if [ $# -ge 1 ]; then
+        show_usage
+    fi
+
+    get_ghc_triplet
+
+    envfn="${ENVSDIR}${envname}"
+
+    if [ -f "$envfn" ]; then
+        echo "removing '$envfn'"
+        rm "$envfn"
+    else
+        echo "Environemnt file '$envfn' doesn't exist; nothing to do"
+    fi
+
+    exit 0
+fi
 
 if [ $# -lt 1 ]; then
     show_usage
@@ -49,11 +112,11 @@ name: z
 version: 0
 cabal-version:>=1.8
 build-type: Simple
- 
+
 library
   build-depends: base
 EOF
-   
+
   for DEP in "$@";
   do
       echo "  build-depends: $DEP"
